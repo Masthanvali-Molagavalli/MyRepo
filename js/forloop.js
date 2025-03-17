76dec54b5f418a37a2a63e93e6e69850e17ee4a7
masthanvali-molagavalli <masthanvali.molagavalli@gmail.com>
2025-03-17 14:34:49 +0530

    added project-06 has been done

---
+++ js/forloop.js
@@ -0,0 +1,21 @@
+let l = "Mastanvali";
+
+for ( i=0; i<=3; i++){
+    console.log(l)
+}
+console.log("loop over");
+
+
+// var v = "Vani";
+// var w=0;
+// while(w<=3){
+//     console.log(v);
+//     w++;
+// }
+
+// var d = "devil";
+// var s = 0;
+// do{
+//     console.log(d);
+//     s++;   
+// }while(s<=10);
